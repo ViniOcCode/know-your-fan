d7940c505ddc03a5558425af98c33ec7392e62e0
vinicius <viniciusoctavio@hotmail.com>
2025-05-03 22:01:26 -0300

    braindamage

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ app = create_app()
 
 if __name__ == '__main__':
     from waitress import serve
-    port = int(os.environ.get('PORT', 8080))  # pega a porta do ambiente, ou usa 8080 como fallback local
+    port = int(os.environ.get('PORT', 5000))  # pega a porta do ambiente, ou usa 8080 como fallback local
     serve(app, host="0.0.0.0", port=port)
