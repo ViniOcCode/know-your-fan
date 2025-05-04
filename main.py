a2629fe2fa5b2c70d404ffc04deee561f8d40d8f
vinicius <viniciusoctavio@hotmail.com>
2025-05-03 22:30:44 -0300

    gunicorn test

--- main.py
+++ main.py
@@ -4,7 +4,4 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    from waitress import serve
-    port = int(os.environ.get('PORT', 5000))  # pega a porta do ambiente, ou usa 8080 como fallback local
-    print(f"Starting server on port {port}...")
-    serve(app, host="0.0.0.0", port=port)
+    app.run()
