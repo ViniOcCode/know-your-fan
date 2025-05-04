f34fd9d3041cf41c87e331fee7aaa40e881de79c
vinicius <viniciusoctavio@hotmail.com>
2025-05-03 22:19:33 -0300

    deploy debugging

--- main.py
+++ main.py
@@ -6,4 +6,5 @@ app = create_app()
 if __name__ == '__main__':
     from waitress import serve
     port = int(os.environ.get('PORT', 5000))  # pega a porta do ambiente, ou usa 8080 como fallback local
+    print(f"Starting server on port {port}...")
     serve(app, host="0.0.0.0", port=port)
