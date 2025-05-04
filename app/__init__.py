641997bbc49818d6912950545a21b67be9d1e6a7
vinicius <viniciusoctavio@hotmail.com>
2025-05-03 22:49:05 -0300

    fix: error with wrong path in sqlite

--- app/__init__.py
+++ app/__init__.py
@@ -8,11 +8,14 @@ def create_app():
     myapp = Flask(__name__)
 
     from app.controllers.data import data
-
     myapp.register_blueprint(data, url_prefix='/')
 
     base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
     db_path = os.path.join(base_dir, 'database', 'database.db')
+
+    if not os.path.exists(os.path.dirname(db_path)):
+        os.makedirs(os.path.dirname(db_path))
+
     myapp.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
     myapp.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -20,4 +23,5 @@ def create_app():
 
     with myapp.app_context():
         db.create_all()
+
     return myapp
