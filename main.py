6cce91ee045390581b92dd990e8131e7e299d7d4
vinicius <viniciusoctavio@hotmail.com>
2025-05-03 21:48:50 -0300

    fix: declared port for render

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
     from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    port = int(os.environ.get('PORT', 8080))  # pega a porta do ambiente, ou usa 8080 como fallback local
+    serve(app, host="0.0.0.0", port=port)
